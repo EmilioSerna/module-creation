583432d1b2797d4252eba01d3ad45dfed12f379a
Angel Emilio Serna Navarro <emilio@vauxoo.com>
2022-07-13 12:21:13 -0700

    [IMP] open_academy: course model creation as example T#59063

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Open Academy",
+
+    "summary": "Exercise of creation of a module",
+
+    "author": "Emilio Serna, Vauxoo",
+    "website": "https://github.com/EmilioSerna/open_academy",
+
+    "license": "LGPL-3",
+
+    "category": "Customizations",
+    "version": "15.0.1.0.0",
+
+    "depends": [
+        "base"
+    ],
+}
