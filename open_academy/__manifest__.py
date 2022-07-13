e46398f2c5b1babc85ccdece2f4a46459c790df7
Angel Emilio Serna Navarro <emilio@vauxoo.com>
2022-07-13 12:21:13 -0700

    [ADD] open-academy: creation of an empty module

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': "Exercise of creation of a module",
+
+    'author': "Emilio Serna, Vauxoo",
+    'website': "https://github.com/EmilioSerna/module-creation",
+
+    'category': 'Uncategorized',
+    'version': '15.0',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
