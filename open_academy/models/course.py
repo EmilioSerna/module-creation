5283fc095be59af1796685c117e8141d92f22700
Angel Emilio Serna Navarro <emilio@vauxoo.com>
2022-07-25 11:37:09 -0700

    [IMP] open_academy: modified copy method course T#59063
    Copy method re-implemented, simply changing the title default to "Copy of [original title]"

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -9,3 +9,8 @@ class Course(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one("res.users", "Responsible")
     session_ids = fields.One2many("session", "course_id")
+
+    def copy(self):
+        default = {"title": f"Copy of {self.title}"}
+
+        return super(Course, self).copy(default)
